3613d3f3553cd2c416e9cc6fa06965f48199312d
Luiz Fernando Santos Silva <luiz.silva@meza.digital>
2021-09-02 20:25:58 -0300

    Web crawler for vaccination schedule

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto("https://agendamentocotia.saudesimples.net/");
+  page.setDefaultNavigationTimeout(120000);
+  await page.click('[id="inputUnchecked"]');
+  await page.click('button');
+  await page.waitForNavigation();
+  await page.type('[name="input_login"]', process.env.HEALTH_LOGIN);
+  await page.type('[name="input_birthday"]', process.env.HEALTH_BIRTHDAY);
+  await page.click('button[type="submit"]');
+  await page.waitForNavigation();
+  await page.waitForSelector('div[class="media"]');
+  await page.click('div[class="media"]');
+  await page.waitForFunction("document.getElementsByClassName('media').length === 2");
+  await page.evaluate(() => {
+    let elements = document.getElementsByClassName('media');
+    elements.item(1).click();
+  });
+  await page.waitForSelector('div[class="placeholder_vagas mx-auto mt-4"]');
+  console.log("Doses esgotadas neste momento, aguardar abertura de nova agenda")
+
+  browser.close()
+})()
